cd7d276e9cc4f9ea7ed96b5db591894a99351216
ishu-dahi <97476476+ishu-dahi@users.noreply.github.com>
2023-05-28 11:24:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "my-aws-tf-state-bucket-ishu"
     key = "main"
-    region = "us-east-1"
+    region = "eu-central-1"
     dynamodb_table = "my-dynamo-db-table"
   }
 }
